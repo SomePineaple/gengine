72adcf4124484988056b9df92dc924aab2736bd6
SomePineaple <njweb@protonmail.com>
2022-01-24 11:34:13 -0500

    More mesh stuff

--- engine/mesh/mesh.go
+++ engine/mesh/mesh.go
@@ -17,12 +17,35 @@ func NewMesh(vertices []float32, textCoords []float32, normals []float32, indice
 	gl.GenVertexArrays(1, &mesh.vaoID)
 	gl.BindVertexArray(mesh.vaoID)
 
-	// Position VBO
+	// Vertices VBO
 	var vboID uint32
 	gl.GenBuffers(1, &vboID)
 	mesh.vboIDList = append(mesh.vboIDList, vboID)
 	gl.BindBuffer(gl.ARRAY_BUFFER, vboID)
 	gl.BufferData(gl.ARRAY_BUFFER, len(vertices)*4, gl.Ptr(vertices), gl.STATIC_DRAW)
+	gl.EnableVertexAttribArray(0)
+	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 0, nil)
+
+	// Texture coordinates VBO
+	gl.GenBuffers(1, &vboID)
+	mesh.vboIDList = append(mesh.vboIDList, vboID)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vboID)
+	gl.BufferData(gl.ARRAY_BUFFER, len(textCoords)*4, gl.Ptr(textCoords), gl.STATIC_DRAW)
+	gl.EnableVertexAttribArray(1)
+	gl.VertexAttribPointer(1, 2, gl.FLOAT, false, 0, nil)
+
+	// Vertex normals VBO
+	gl.GenBuffers(1, &vboID)
+	mesh.vboIDList = append(mesh.vboIDList, vboID)
+	gl.BindBuffer(gl.ARRAY_BUFFER, vboID)
+	gl.BufferData(gl.ARRAY_BUFFER, len(normals)*4, gl.Ptr(normals), gl.STATIC_DRAW)
+	gl.EnableVertexAttribArray(2)
+	gl.VertexAttribPointer(2, 3, gl.FLOAT, false, 0, nil)
+
+	// Index VBO
+	gl.GenBuffers(1, &vboID)
+	mesh.vboIDList = append(mesh.vboIDList, vboID)
+	gl.BindBuffer(gl.ELEMENT_ARRAY_BUFFER, vboID)
 
 	return mesh
 }
